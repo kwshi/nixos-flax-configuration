f1af211d4bd3348ea0bb5d4ae8568b8643c617b8
Kye Shi <kiwishi@pm.me>
2023-02-13 18:23:40 -0800

    git: use pm.me email address

--- home/profiles/git.nix
+++ home/profiles/git.nix
@@ -2,7 +2,7 @@
   programs.git = {
     enable = true;
     userName = "Kye Shi";
-    userEmail = "kiwishi@proton.me";
+    userEmail = "kiwishi@pm.me";
     signing = {
       key = "69B1A859";
       signByDefault = true;
